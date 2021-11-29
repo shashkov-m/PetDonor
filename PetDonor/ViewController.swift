113324d3060b9af3dfb34ff4d81b54a6eec7a840
shashkov-m <43266908+shashkov-m@users.noreply.github.com>
2021-11-29 13:41:29 +0300

    Initial Commit

--- PetDonor/ViewController.swift
+++ PetDonor/ViewController.swift
@@ -2,18 +2,18 @@
 //  ViewController.swift
 //  PetDonor
 //
-//  Created by 18261451 on 29.11.2021.
+//  Created by Shashkov Max on 29.11.2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-
+  
+  
 }
 
