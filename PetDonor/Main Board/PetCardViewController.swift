f72b4ae77cb7f4317b36ac6448dfb22ede149f76
Max <ishashkov.m@gmail.com>
2022-02-07 00:45:51 +0300

    1.0

--- PetDonor/Main Board/PetCardViewController.swift
+++ PetDonor/Main Board/PetCardViewController.swift
@@ -9,15 +9,17 @@ import UIKit
 
 class PetCardViewController: UIViewController {
   var pet:Pet?
+  let formatter = DateFormatter ()
   @IBOutlet weak var petTypeLabel: UILabel!
   @IBOutlet weak var bloodTypeLabel: UILabel!
   @IBOutlet weak var postTypeLabel: UILabel!
   @IBOutlet weak var petImageView: UIImageView!
   @IBOutlet weak var cityLabel: UILabel!
-  @IBOutlet weak var petInfoLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
   @IBOutlet weak var contactInfoLabel: UILabel!
   @IBOutlet weak var dateCreateLabel: UILabel!
   @IBOutlet weak var rewardLabel: UILabel!
+  @IBOutlet weak var ageLabel: UILabel!
   
   
   override func viewDidLoad() {
@@ -26,18 +28,18 @@ class PetCardViewController: UIViewController {
   }
   
   private func viewConfigure () {
-    guard let pet = pet, let dateCreate = pet.dateCreate, let bloodType = pet.bloodType else {
-      return
+    guard let pet = pet else { return }
+    if let dateCreate = pet.dateCreate {
+      formatter.dateFormat = "dd.MM.yyyy hh:mm"
+      let date = formatter.string(from: dateCreate)
+      dateCreateLabel.text = date
     }
-    let formatter = DateFormatter ()
-    formatter.dateFormat = "dd.MM.yyyy hh:mm"
-    let date = formatter.string(from: dateCreate)
+    bloodTypeLabel.text = pet.bloodType
     petTypeLabel.text = pet.petType?.rawValue
-    bloodTypeLabel.text = "Группа крови \(bloodType)"
     postTypeLabel.text = pet.postType
     cityLabel.text = pet.city?.title
-    petInfoLabel.text = pet.description
+    descriptionLabel.text = pet.description
     contactInfoLabel.text = pet.contactInfo
-    dateCreateLabel.text = "Дата публикации \(date)"
+    ageLabel.text = pet.age
   }
 }
--- PetDonor/Model/Database.swift
+++ PetDonor/Model/Database.swift
@@ -124,14 +124,37 @@ final class Database {
         var age = birthDate
         let calendar = Calendar.current
         let formatter = DateFormatter ()
+        formatter.dateFormat = "dd.MM.yyyy"
         if let tmpAge = formatter.date(from: birthDate) {
           let components = calendar.dateComponents([.year, .month], from: tmpAge, to: Date ())
-          age = "\(components.month ?? 0) месяцев"
+          age = ruDatePlural(year: components.year, month: components.month)
         }
         let pet = Pet (city: city, description: description, contactInfo: contactInfo, bloodType: bloodType, postType: postType, petType: PetType.init(rawValue: petType), isVisible: isVisible, userID: userID, dateCreate: date, reward: reward, age: age, imageUrl: imageUrl)
         array.append(pet)
       }
     }
     return array
+  }
+  func ruDatePlural (year:Int?, month:Int?) -> String {
+    var ruYears = "год"
+    var ruMonths = "месяц"
+    switch year {
+    case 1:
+      break
+    case 2,3,4:
+      ruYears += "а"
+    default:
+      ruYears = "лет"
+    }
+    switch month {
+    case 1:
+      break
+    case 2,3,4:
+      ruMonths += "а"
+    default:
+      ruMonths += "ев"
+    }
+    let result = "\(year ?? 0) \(ruYears), \(month ?? 0) \(ruMonths)"
+    return result
   }
 }
